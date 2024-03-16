8ca5c649763610714b9227ac39a4e6c51e3935af
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-03-16 20:02:46 +0200

    added get by id route

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,25 @@
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
+import HttpError from "../helpers/HttpError.js";
 import contactsServices from "../services/contactsServices.js";
 
 const getAll = async (req, res) => {
   const result = await contactsServices.listContacts();
+
+  res.json(result);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsServices.getContactById(id);
+
+  if (!result) {
+    throw HttpError(404);
+  }
+
   res.json(result);
 };
 
 export default {
   getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
 };
--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -4,7 +4,7 @@ import contactsControllers from "../../controllers/contactsControllers.js";
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsControllers.getAll);
-contactsRouter.get("/:id", (req, res) => {});
+contactsRouter.get("/:id", contactsControllers.getById);
 contactsRouter.delete("/:id", (req, res) => {});
 contactsRouter.post("/", (req, res) => {});
 contactsRouter.put("/:id", (req, res) => {});
